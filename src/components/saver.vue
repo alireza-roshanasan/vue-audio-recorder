4221ae121e60e8e4091125c58d7f8a9ade205e91
alireza <alireza.roshanasan@gmail.com>
2020-11-08 00:41:39 +0330

    add save component

---
+++ src/components/saver.vue
@@ -0,0 +1,27 @@
+<style lang="scss">
+  @import '../scss/icons';
+</style>
+
+<template>
+  <icon-button name="tick" class="ar-icon ar-icon__xs ar-icon--no-border" @click.native="save"/>
+</template>
+
+<script>
+  import IconButton from './icon-button'
+  export default {
+    props: {
+      record: { type: Object }
+    },
+    components: {
+      IconButton
+    },
+    methods: {
+      save () {
+        if (!this.record.url) {
+          return
+        }
+        this.$eventBus.$emit('save-upload', this.record.blob)
+      }
+    }
+  }
+</script>
